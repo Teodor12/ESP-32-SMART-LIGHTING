740bbb4c62a3847596df65618582091465e029db
Teodor <fodor.teo@gmail.com>
2024-04-06 18:32:10 +0000

    validate wakeword detection

--- main/main.c
+++ main/main.c
@@ -3,13 +3,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include <bsp.h>
+#include "voice_assistant.h"
 
 #define TAG "main"
 
-int app_main(void)
+void app_main(void)
 {
-    ESP_ERROR_CHECK(bsp_board_init(16000, 2, 32));
-    ESP_LOGI(TAG, "I2S initialized successfully!");
-    return 0;
+    start_voice_assistant();
 }
